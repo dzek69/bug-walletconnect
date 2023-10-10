37efaaf81ae10061d6faf3732cefeac13dc198b6
dzek <git-public@dzek.eu>
2023-10-10 19:21:12 +0200

    bug repro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+
+import { WagmiConfig } from 'wagmi'
+import { bsc, mainnet} from 'wagmi/chains'
+
+// 1. Get projectId
+const projectId = '7621cde10c7a3a149fb7b5b31e90a530'
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
+
+const chains = [mainnet, bsc]
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+// 3. Create modal
+createWeb3Modal({ wagmiConfig, projectId, chains })
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+      <WagmiConfig config={wagmiConfig}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+  )
+}
+
